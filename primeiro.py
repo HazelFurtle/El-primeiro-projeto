6ec4302ddba8dbf31535eebdf8528f6ab9a6e02a
Furtle <guilhermesbarbosa1008@gmail.com>
2026-06-06 17:31:31 -0300

    threw the old stuff in the bin and added the bot code
    lçmkdlkawnçkdcnaw.ckçmawdc

--- primeiro.py
+++ primeiro.py
@@ -1,17 +1,40 @@
-meme_dict = {
-            "CRINGE": "Algo vergonhoso ou constrangedor",
-            "STALKEAR": "Investigar a vida de alguém online",
-            "LEONARDO": "Ser humano deste planeta terra"
-            }
+import discord
+import random
 
-word = input("Digite uma palavra moderna que você não entende (escreva todo a palavra em letras maiúsculas): ")
+from discord.ext import commands
 
-if word in meme_dict.keys():
-    print(meme_dict[word])
-    # O que devemos fazer se a palavra for encontrada?
-    
-else:
-    print("num existe :(")
-    # O que devemos fazer se a palavra não for encontrada?
+intents = discord.Intents.default()
+intents.message_content = True
 
-# :)
+bot = commands.Bot(command_prefix='$', intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f'Estamos logados como {bot.user}')
+
+@bot.command()
+async def hello(ctx):
+    await ctx.send(f'Olá! eu sou um bot {bot.user}!')
+
+@bot.command()
+async def heh(ctx, count_heh = 5):
+    await ctx.send("he" * count_heh)
+
+@bot.command()
+async def roll(ctx, dice: str):
+    """Rolls a dice in NdN format."""
+    try:
+        rolls, limit = map(int, dice.split('d'))
+    except Exception:
+        await ctx.send('Format has to be in NdN!')
+        return
+
+    result = ', '.join(str(random.randint(1, limit)) for r in range(rolls))
+    await ctx.send(result)
+
+@bot.command()
+async def add(ctx, left: int, right: int):
+    """Adds two numbers together."""
+    await ctx.send(left + right)
+
+bot.run("MAHTOKEN")
